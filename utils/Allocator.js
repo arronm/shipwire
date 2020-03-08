8f45948eb049aaaf38180c3c7019ebdaad2e5ca6
Arron Marshall <marshall.arron@gmail.com>
2020-03-07 22:40:45 -0800

    Refactor Allocator to use events

--- utils/Allocator.js
+++ utils/Allocator.js
@@ -1,36 +1,31 @@
-const PriorityQueue = require('./PriorityQueue');
-
-
-// Set up job queue
-const queue = new PriorityQueue(); // this can be changed to database table
-
-// Get total inventory
-
-// Pull next job from queue
-
-// Process Order
-
-// Push inventory to transaction after handled
-
-
-
-
-/* ---------------- */
-
-
 /*
 
 Only valid orders should be added to the job queue
 
 */
-
+const EventEmitter = require('events');
 const productDB = require('../api/product.model');
 
 class Allocator {
   constructor() {
     this.startedAt = Date.now();
-    this.inventory = null;
+    this.inventory = undefined;
+    this.task = undefined;
+    this.event = new EventEmitter();
     this.tmp = ['one', 'two', 'three'];
+
+    this.productInventory = {
+      "A": 100,
+      "B": 150,
+    }
+
+    this.event.on('QueueProcessed', async () => {
+      await allocator.watchQueue();
+    });
+
+    this.event.on('QueueRefreshed', async () => {
+      await allocator.processQueue();
+    });
   }
 
   getTask() {
@@ -51,35 +46,42 @@ class Allocator {
   }
 
   async processQueue() {
-    // Get inventory if hasn't been grabbed yet
-    this.inventory = this.inventory || await productDB.getTotalInventory();
-    let task = this.getTask();
-
-    while (task) {
+    while (this.task) {
       // Sleep 1 second between tasks
       await new Promise(resolve => setTimeout(resolve, 1000));
 
       // Handle task
-      console.log('HANDLE TASK', task);
+      console.log('HANDLE TASK', this.task);
+      this.inventory -= 1;
 
       // set next task
-      task = this.getTask();
+      this.task = this.getTask();
     }
+
+    this.event.emit('QueueProcessed');
   }
 
-  async run() {
-    while (true) {
-      // process queue
-      await this.processQueue();
-
-      // recheck the queue every 5 seconds
+  async watchQueue() {
+    while (!this.task) {
+      // Check the queue every 5 seconds
       await new Promise(resolve => setTimeout(resolve, 5000));
+      this.task = this.getTask();
     }
+
+    this.event.emit('QueueRefreshed');
+  }
+
+  async start() {
+    // get total inventory
+    this.inventory = await productDB.getTotalInventory();
+
+    // set up initial task
+    this.task = this.getTask();
+    this.processQueue();
   }
 }
 
 const allocator = new Allocator();
 console.log('Started at:', allocator.startedAt);
 
-allocator.run();
-// module.exports = Allocator;
+allocator.start();
