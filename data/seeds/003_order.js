06d999a959f1f08aa1a2aeb6bd2bdb390269fc8d
Arron Marshall <marshall.arron@gmail.com>
2020-03-08 11:42:41 -0700

    Add order seed

---
+++ data/seeds/003_order.js
@@ -0,0 +1,22 @@
+exports.seed = async (knex) => {
+  await knex('stream').insert([
+    {
+      stream_id: 1
+    },
+    {
+      stream_id: 2
+    },
+    {
+      stream_id: 3
+    },
+    {
+      stream_id: 4
+    },
+    {
+      stream_id: 5
+    },
+    {
+      stream_id: 6
+    },
+  ]);
+};
