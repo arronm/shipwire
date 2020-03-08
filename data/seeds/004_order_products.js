10a39ed0dd97fc8769c0d060e35354b707f06ad5
Arron Marshall <marshall.arron@gmail.com>
2020-03-08 11:43:10 -0700

    Add order_products seed

---
+++ data/seeds/004_order_products.js
@@ -0,0 +1,44 @@
+exports.seed = async (knex) => {
+  await knex('product').insert([
+    {
+      order_id: 1,
+      product_id: 1,
+      quantity: 1,
+    },
+    {
+      order_id: 1,
+      product_id: 3,
+      quantity: 1,
+    },
+    {
+      order_id: 2,
+      product_id: 5,
+      quantity: 5,
+    },
+    {
+      order_id: 3,
+      product_id: 4,
+      quantity: 4,
+    },
+    {
+      order_id: 4,
+      product_id: 1,
+      quantity: 1,
+    },
+    {
+      order_id: 4,
+      product_id: 3,
+      quantity: 1,
+    },
+    {
+      order_id: 5,
+      product_id: 2,
+      quantity: 3,
+    },
+    {
+      order_id: 6,
+      product_id: 4,
+      quantity: 4,
+    },
+  ]);
+};
