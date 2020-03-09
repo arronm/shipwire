a09a9e6163dffb9c30f9b671ad3509ccaa4e5bd4
Arron Marshall <marshall.arron@gmail.com>
2020-03-09 12:41:13 -0700

    Set up streams to create orders

--- utils/DataSource.js
+++ utils/DataSource.js
@@ -1,6 +1,4 @@
 // Must be capable of generating one or more streams of orders
-const faker = require('faker');
-
 const randRange = (min, max) => Math.floor((Math.random() * max) + min);
 
 const numStreams = 6;
@@ -19,7 +17,6 @@ const GenerateOrderData = () => {
 
   const lines = []
   const lineProducts = [...products]; // make a copy of the products array
-  // console.log('lineProducts', lineProducts)
 
   for (let i = 1; i <= numProducts; i++) {
     lines.push({
@@ -43,5 +40,69 @@ const GenerateOrderData = () => {
 
 // Create Stream (spawn process)
 
+class Stream {
+  constructor(id) {
+    this.id = id;
+  }
 
-GenerateOrderData();
+  generateOrderData() {
+    const numProducts = randRange(1, products.length);
+
+    const lines = []
+    const lineProducts = [...products];
+
+    for (let i = 1; i <= numProducts; i++) {
+      lines.push({
+        product: lineProducts.splice(randRange(1, lineProducts.length) - 1, 1)[0],
+        quantity: randRange(1, 5),
+      });
+    };
+
+    const order = {
+      stream_id: this.id,
+      header: this.id.toString(),
+      lines,
+    };
+
+    return order;
+  }
+
+  async createOrder() {
+    // TODO: option to manually create orders in database (order, order_products, job)
+  }
+
+  async sendOrders() {
+    // TODO: pass optional number of orders to generate to constructor
+    // TODO: set up axios to send orders through endpoint
+    for (let i = 0; i <= 25; i++) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      // console.log(`Sending order #${i} from Stream ${this.id}`);
+      console.log(this.generateOrderData());
+    }
+  }
+}
+
+// data source can create and spawn streams?
+class DataSource {
+  constructor(test) {
+    this.test = test;
+    this.streams = []
+  }
+
+  createStream(streamId) {
+    // const streamId = randRange(1, numStreams);
+    const stream = new Stream(streamId);
+    this.streams.push(stream);
+    stream.sendOrders();
+  }
+
+  createStreams() {
+    // TODO: Spawn based on numStreams
+    for (let i = 1; i < 3; i++) {
+      this.createStream(i);
+    }
+  }
+}
+
+const data = new DataSource('abc');
+data.createStreams();
